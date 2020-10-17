a9a90f65140ee1da776f4eca832ac50f849a623b
Fathorrosi <fathorrosi1777@gmail.com>
2020-10-17 20:34:12 +0700

    little changes

--- soal1.py
+++ soal1.py
@@ -16,13 +16,13 @@ class Tabung:
     def luasPermukaan(self):
         lp = 2 * self.phi * self.r * (self.r + self.t)
         return lp
-        
+
     def luasSelimut(self):
         ls = 2 * self.phi * self.r * self.t
         return ls
+
 
 tabung1 = Tabung(7, 17)
 tabung2 = Tabung(2, 7)
 print("volume = ", tabung1.luasPermukaan())
 print("Jumlah objek yang dibuat :", Tabung.counter)
-
